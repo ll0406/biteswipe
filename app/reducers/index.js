19f882e2416a84f94b3a0e334503cc88f33e81de
BuildTools <dennisdeng2002@yahoo.com>
2017-01-26 15:59:40 -0500

    add restaurants reducer, action-creator

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({
-	login: require('./login').default
+	login: require('./login').default,
+	restaurants: require('./restaurants').default
 });
 
 export default rootReducer;
