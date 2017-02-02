629fd9c74d2a6c46bde5eaa89dcc0084baa1f35f
rachel-rocks-at-ruby <rachelb@gmail.com>
2017-02-02 09:17:56 -0800

    delete app file

--- app/constants.jsx
+++
@@ -1,7 +0,0 @@
-// auth
-export const AUTHENTICATED = 'AUTHENTICATED';
-
-// restaurants
-export const RECEIVE_RESTAURANTS = 'RECEIVE_RESTAURANTS';
-
-export const RECEIVE_LOCATION = 'RECEIVE_LOCATION';
